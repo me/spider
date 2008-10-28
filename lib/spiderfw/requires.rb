3e2f74ace6b5b29ca1bc179de75636372b2829a5
Ivan <ivan.pirlik@gmail.com>
2008-10-28 16:06:42 +0100

    Aggiunto Symbol::to_proc

--- lib/spiderfw/requires.rb
+++ lib/spiderfw/requires.rb
@@ -1,6 +1,7 @@
 require 'ruby-debug'
 require 'spiderfw/utils/monkey/kernel'
 require 'spiderfw/utils/monkey/class'
+require 'spiderfw/utils/monkey/symbol'
 require 'spiderfw/utils/inflector'
 require 'spiderfw/i18n/tmp_gettext'
 require 'spiderfw/config/configuration'
