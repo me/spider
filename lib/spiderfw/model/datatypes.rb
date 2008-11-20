7692e96020841927670c2e8a7a2242f1f4e76bbb
Ivan <ivan.pirlik@gmail.com>
2008-11-20 22:43:42 +0100

    Aggiunta classe DataType

--- lib/spiderfw/model/datatypes.rb
+++ lib/spiderfw/model/datatypes.rb
@@ -1,8 +1,40 @@
 module Spider
     
     module DataTypes
+        
+        class DataType
+            @maps_to = 'text'
+            
+            def initialize(val=nil)
+                set(val) if val
+            end
+            
+            def self.maps_to(val=nil)
+                if (val)
+                    @maps_to = val
+                else
+                    @maps_to
+                end
+            end
+            
+            def set(val)
+                @val = val
+            end
+            
+            def serialize(val)
+                val
+            end
+            
+            def unserialize(val)
+                val
+            end
+                
+        end
+        
         autoload(:Bool, 'spiderfw/model/datatypes/bool')
         autoload(:Text, 'spiderfw/model/datatypes/text')
+        
+        
     end
     
 end
