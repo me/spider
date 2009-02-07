70de0294759f28151a2947b93148e057a8aaabeb
Ivan <abmajor7@gmail.com>
2009-02-07 13:21:07 +0100

    Changed sync and synchronized

--- lib/spiderfw/model/sync.rb
+++ lib/spiderfw/model/sync.rb
@@ -50,6 +50,7 @@ module Spider; module Model
         end
         
         def collect_dependencies(model)
+            return collect_dependencies(model.superclass) if model.attributes[:inherit_storage]
             @processed_deps[model] = true
             @model_tasks[model] ||= SyncTask.new(model)
             model.elements_array.select{ |el| el.model? && model.mapper.have_references?(el) }.each do |el|
