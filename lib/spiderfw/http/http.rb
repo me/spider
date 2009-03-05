2497f95a4622d1a8f80cc76d44368f423c690af3
Ivan <abmajor7@gmail.com>
2009-03-05 14:57:35 +0100

    Moved status codes to submodule

--- lib/spiderfw/http/http.rb
+++ lib/spiderfw/http/http.rb
@@ -8,110 +8,120 @@ module Spider
         
         @multipart_regexp = /\Amultipart\/form-data.*boundary=\"?([^\";,]+)/n.freeze
         
-        CONTINUE = 100
-        SWITCHING_PROTOCOLS = 101
-        WEBDAV_PROCESSING = 102
-        OK = 200
-        CREATED = 201
-        ACCEPTED = 202
-        NON_AUTHORITATIVE = 203
-        NO_CONTENT = 204
-        RESET_CONTENT = 205
-        PARTIAL_CONTENT = 206
-        WEBDAV_MULTI_STATUS = 207
-        MULTIPLE_CHOICES = 300
-        MOVED_PERMANENTLY = 301
-        FOUND = 302
-        SEE_OTHER = 303
-        NOT_MODIFIED = 304
-        USE_PROXY = 305
-        TEMPORARY_REDIRECT = 307
-        BAD_REQUEST = 400
-        UNAUTHORIZED = 401
-        FORBIDDEN = 403
-        NOT_FOUND = 404
-        METHOD_NOT_ALLOWED = 405
-        NOT_ACCEPTABLE = 406
-        PROXY_AUTHENTICATION_REQUIRED = 407
-        REQUEST_TIMEOUT = 408
-        CONFLICT = 409
-        GONE = 410
-        LENGTH_REQUIRED = 411
-        PRECONDITION_FAILED = 412
-        REQUEST_ENTITY_TOO_LARGE = 413
-        REQUEST_URI_TOO_LONG = 414
-        UNSUPPORTED_MEDIA_TYPE = 415
-        REQUESTED_RANGE_NOT_SATISFIABLE = 416
-        EXPECTATION_FAILED = 417
-        WEBDAV_UNPROCESSABLE_ENTITY = 422
-        WEBDAV_LOCKED = 423
-        WEBDAV_FAILED_DEPENDENCY = 424
-        UPGRADE_REQUIRED = 426
-        INTERNAL_SERVER_ERROR = 500
-        NOT_IMPLEMENTED = 501
-        BAD_GATEWAY = 502
-        SERVICE_UNAVAILABLE = 503
-        GATEWAY_TIMEOUT = 504
-        HTTP_VERSION_NOT_SUPPORTED = 505
-        VARIANT_ALSO_NEGOTIATES = 506
-        WEBDAV_INSUFFICIENT_STORAGE = 507
-        BANDWIDTH_LIMIT_EXCEEDED = 509
-        NOT_EXTENDED = 510
+        module StatusCodes
+        
+            CONTINUE = 100
+            SWITCHING_PROTOCOLS = 101
+            WEBDAV_PROCESSING = 102
+            OK = 200
+            CREATED = 201
+            ACCEPTED = 202
+            NON_AUTHORITATIVE = 203
+            NO_CONTENT = 204
+            RESET_CONTENT = 205
+            PARTIAL_CONTENT = 206
+            WEBDAV_MULTI_STATUS = 207
+            MULTIPLE_CHOICES = 300
+            MOVED_PERMANENTLY = 301
+            FOUND = 302
+            SEE_OTHER = 303
+            NOT_MODIFIED = 304
+            USE_PROXY = 305
+            TEMPORARY_REDIRECT = 307
+            BAD_REQUEST = 400
+            UNAUTHORIZED = 401
+            FORBIDDEN = 403
+            NOT_FOUND = 404
+            METHOD_NOT_ALLOWED = 405
+            NOT_ACCEPTABLE = 406
+            PROXY_AUTHENTICATION_REQUIRED = 407
+            REQUEST_TIMEOUT = 408
+            CONFLICT = 409
+            GONE = 410
+            LENGTH_REQUIRED = 411
+            PRECONDITION_FAILED = 412
+            REQUEST_ENTITY_TOO_LARGE = 413
+            REQUEST_URI_TOO_LONG = 414
+            UNSUPPORTED_MEDIA_TYPE = 415
+            REQUESTED_RANGE_NOT_SATISFIABLE = 416
+            EXPECTATION_FAILED = 417
+            WEBDAV_UNPROCESSABLE_ENTITY = 422
+            WEBDAV_LOCKED = 423
+            WEBDAV_FAILED_DEPENDENCY = 424
+            UPGRADE_REQUIRED = 426
+            INTERNAL_SERVER_ERROR = 500
+            NOT_IMPLEMENTED = 501
+            BAD_GATEWAY = 502
+            SERVICE_UNAVAILABLE = 503
+            GATEWAY_TIMEOUT = 504
+            HTTP_VERSION_NOT_SUPPORTED = 505
+            VARIANT_ALSO_NEGOTIATES = 506
+            WEBDAV_INSUFFICIENT_STORAGE = 507
+            BANDWIDTH_LIMIT_EXCEEDED = 509
+            NOT_EXTENDED = 510
 
-        @status_messages = {
-            CONTINUE => 'Continue',
-            SWITCHING_PROTOCOLS => 'Switching Protocols',
-            WEBDAV_PROCESSING => 'Processing',
-            OK => 'OK',
-            CREATED => 'Created',
-            ACCEPTED => 'Accepted',
-            NON_AUTHORITATIVE => 'Non Authoritative',
-            NO_CONTENT => 'No Content',
-            RESET_CONTENT => 'Reset Content',
-            PARTIAL_CONTENT => 'Partial Content',
-            WEBDAV_MULTI_STATUS => 'Multi-Status',
-            MULTIPLE_CHOICES => 'Multiple Choices',
-            MOVED_PERMANENTLY => 'Moved Permanently',
-            FOUND => 'Found',
-            SEE_OTHER => 'See Other',
-            NOT_MODIFIED => 'Not Modified',
-            USE_PROXY => 'Use Proxy',
-            TEMPORARY_REDIRECT => 'Temporary Redirect',
-            BAD_REQUEST => 'Bad Request',
-            UNAUTHORIZED => 'Unauthorized',
-            FORBIDDEN => 'Forbidden',
-            NOT_FOUND => 'Not Found',
-            METHOD_NOT_ALLOWED => 'Method Not Allowed',
-            NOT_ACCEPTABLE => 'Not Acceptable',
-            PROXY_AUTHENTICATION_REQUIRED => 'Proxy Authentication Required',
-            REQUEST_TIMEOUT => 'Request Timeout',
-            CONFLICT => 'Conflict',
-            GONE => 'Gone',
-            LENGTH_REQUIRED => 'Length Required',
-            PRECONDITION_FAILED => 'Precondition Failed',
-            REQUEST_ENTITY_TOO_LARGE => 'Request Entity Too Large',
-            REQUEST_URI_TOO_LONG => 'Request-URI Too Long',
-            UNSUPPORTED_MEDIA_TYPE => 'Unsupported Media Type',
-            REQUESTED_RANGE_NOT_SATISFIABLE => 'Requested Range Not Satisfiable',
-            EXPECTATION_FAILED => 'Expectation Failed',
-            WEBDAV_UNPROCESSABLE_ENTITY => 'Unprocessable Entity',
-            WEBDAV_LOCKED => 'Locked',
-            WEBDAV_FAILED_DEPENDENCY => 'Failed Dependency',
-            UPGRADE_REQUIRED => 'Upgrade Required',
-            INTERNAL_SERVER_ERROR => 'Internal Server Error',
-            NOT_IMPLEMENTED => 'Not Implemented',
-            BAD_GATEWAY => 'Bad Gateway',
-            SERVICE_UNAVAILABLE => 'Service Unavailable',
-            GATEWAY_TIMEOUT => 'Gateway Timeout',
-            HTTP_VERSION_NOT_SUPPORTED => 'HTTP Version Not Supported',
-            VARIANT_ALSO_NEGOTIATES => 'Variant Also Negotiates',
-            WEBDAV_INSUFFICIENT_STORAGE => 'Insufficient Storage',
-            BANDWIDTH_LIMIT_EXCEEDED => 'Bandwidth Limit Exceeded',
-            NOT_EXTENDED => 'Not Extended'
-        }
+            @status_messages = {
+                CONTINUE => 'Continue',
+                SWITCHING_PROTOCOLS => 'Switching Protocols',
+                WEBDAV_PROCESSING => 'Processing',
+                OK => 'OK',
+                CREATED => 'Created',
+                ACCEPTED => 'Accepted',
+                NON_AUTHORITATIVE => 'Non Authoritative',
+                NO_CONTENT => 'No Content',
+                RESET_CONTENT => 'Reset Content',
+                PARTIAL_CONTENT => 'Partial Content',
+                WEBDAV_MULTI_STATUS => 'Multi-Status',
+                MULTIPLE_CHOICES => 'Multiple Choices',
+                MOVED_PERMANENTLY => 'Moved Permanently',
+                FOUND => 'Found',
+                SEE_OTHER => 'See Other',
+                NOT_MODIFIED => 'Not Modified',
+                USE_PROXY => 'Use Proxy',
+                TEMPORARY_REDIRECT => 'Temporary Redirect',
+                BAD_REQUEST => 'Bad Request',
+                UNAUTHORIZED => 'Unauthorized',
+                FORBIDDEN => 'Forbidden',
+                NOT_FOUND => 'Not Found',
+                METHOD_NOT_ALLOWED => 'Method Not Allowed',
+                NOT_ACCEPTABLE => 'Not Acceptable',
+                PROXY_AUTHENTICATION_REQUIRED => 'Proxy Authentication Required',
+                REQUEST_TIMEOUT => 'Request Timeout',
+                CONFLICT => 'Conflict',
+                GONE => 'Gone',
+                LENGTH_REQUIRED => 'Length Required',
+                PRECONDITION_FAILED => 'Precondition Failed',
+                REQUEST_ENTITY_TOO_LARGE => 'Request Entity Too Large',
+                REQUEST_URI_TOO_LONG => 'Request-URI Too Long',
+                UNSUPPORTED_MEDIA_TYPE => 'Unsupported Media Type',
+                REQUESTED_RANGE_NOT_SATISFIABLE => 'Requested Range Not Satisfiable',
+                EXPECTATION_FAILED => 'Expectation Failed',
+                WEBDAV_UNPROCESSABLE_ENTITY => 'Unprocessable Entity',
+                WEBDAV_LOCKED => 'Locked',
+                WEBDAV_FAILED_DEPENDENCY => 'Failed Dependency',
+                UPGRADE_REQUIRED => 'Upgrade Required',
+                INTERNAL_SERVER_ERROR => 'Internal Server Error',
+                NOT_IMPLEMENTED => 'Not Implemented',
+                BAD_GATEWAY => 'Bad Gateway',
+                SERVICE_UNAVAILABLE => 'Service Unavailable',
+                GATEWAY_TIMEOUT => 'Gateway Timeout',
+                HTTP_VERSION_NOT_SUPPORTED => 'HTTP Version Not Supported',
+                VARIANT_ALSO_NEGOTIATES => 'Variant Also Negotiates',
+                WEBDAV_INSUFFICIENT_STORAGE => 'Insufficient Storage',
+                BANDWIDTH_LIMIT_EXCEEDED => 'Bandwidth Limit Exceeded',
+                NOT_EXTENDED => 'Not Extended'
+            }
+        
+            def self.status_messages
+                @status_messages
+            end
+            
+        end
+        
+        include StatusCodes
         
         def self.status_messages
-            @status_messages
+            StatusCodes.status_messages
         end
         
         # ==== Parameters
