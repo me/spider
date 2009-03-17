cd947ff52343260700ee3186bfdcff690ef520d2
Ivan <abmajor7@gmail.com>
2009-03-18 00:24:35 +0100

    Use app.route_url for routing app (defaults to underscore of app name)

--- lib/spiderfw/controller/spider_controller.rb
+++ lib/spiderfw/controller/spider_controller.rb
@@ -3,10 +3,11 @@ module Spider
     class SpiderController < Controller
         
         def self.route_app(app)
-            Spider::Logger.debug("ROUTING APP #{app.name}")
-            app_path = app.name.gsub('::', '/')
+            #app_path = app.name.gsub('::', '/')
+            app_path = app.route_url
+            Spider::Logger.debug("ROUTING #{app_path} TO #{app.controller}")
             route(app_path, app.controller, :ignore_case => true)
-        end        
+        end
                 
     end
     
