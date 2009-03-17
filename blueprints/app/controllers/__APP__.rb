cf3ddd2ec0a97ee9e04158ff86dcd55cd660e0fd
Ivan <abmajor7@gmail.com>
2009-03-18 00:04:40 +0100

    Blueprint controller named as app

--- blueprints/app/controllers/__APP__.rb
+++ blueprints/app/controllers/__APP__.rb
@@ -1,14 +1,15 @@
-<%= modules.inject(""){ |s, mod| s+= "module #{mod}; "} %>module Controllers
+<%= modules[0..-2].inject(""){ |s, mod| s+= "module #{mod}; "} %>module <%=modules[-1]%>
     
-    class <%= module_name %>Controller < Spider::Controller
+    class <%= module_name %>Controller < Spider::PageController
+        include Spider::Helpers::HTTP, Spider::Helpers::StaticContent
+        
         layout '<%= app_name %>'
     
         def index
-            puts "Rendering template<br>"
             @scene.msg = 'Hello!'
             render('index')
         end
     
     end
     
-<%= modules.inject("") { |str, mod| str+= 'end; '} %>end
+<%= modules[0..-2].inject("") { |str, mod| str+= 'end; '} %>end
