6266a4d15d52702570e0f77e4c956a74908906aa
Ivan <abmajor7@gmail.com>
2009-03-18 00:20:36 +0100

    Removed old code, included StaticContent

--- lib/spiderfw/controller/app_controller.rb
+++ lib/spiderfw/controller/app_controller.rb
@@ -1,25 +1,11 @@
+require 'spiderfw/controller/helpers/static_content'
+
 module Spider
     
     class AppController < Controller
         include Visual
-        
-        no_layout 'serve_resource'
-        
-        def initialize(request, response, scene=nil)
-            route 'pub/', :serve_resource
-            super
-        end
-        
-        
-        def serve_resource(path)
-            full_path = self.class.app.pub_path+'/'+path
-            debug("Serving resource: #{full_path}")
-            raise NotFound.new(path) unless File.exist?(full_path)
-            f = File.open(full_path, 'r')
-            while (block = f.read(1024)) do
-                print block
-            end
-        end
+        include StaticContent
+
         
     end
     
