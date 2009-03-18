31460437a8b7b4461a9b2359ff9ef82eecb628cf
Ivan <abmajor7@gmail.com>
2009-03-18 19:18:48 +0100

    Added sorting to table

--- apps/core/components/widgets/table/table.rb
+++ apps/core/components/widgets/table/table.rb
@@ -15,10 +15,26 @@ module Spider; module Components
                 @page = @page.to_i
                 @offset = ((@page - 1) * @attributes[:row_limit])
             end
+            if params['sort']
+                @sort = params['sort'].to_sym 
+                @page = 1
+            end
+            if (@sort)
+                if (@model.elements[@sort].model?)
+                    s = []
+                    @model.elements[@sort].model.each_element{ |el| s << "#{@sort}.#{el.name}" if el.type == String && !el.primary_key? }
+                    @sort = s
+                end
+            end
+            @sort = session[:sort] if !@sort && session[:sort]
+            session[:sort] = @sort if @sort
+            @sort = [@sort] if @sort && !@sort.is_a?(Array)
         end
         
         def execute
             @scene.elements = @attributes[:elements] || @attributes[:model].elements_array.map{ |el| el.name }
+            @scene.sortable = {}
+            @model.elements_array.each{ |el| @scene.sortable[el.name] = @model.mapper.mapped?(el) ? true : false }
             @scene.labels = {}
             @scene.elements.each do |el|
                 @scene.labels[el] = @model.elements[el].label
@@ -30,6 +46,7 @@ module Spider; module Components
                 @scene.page = @page
                 @scene.paginate = true
             end
+            @scene.rows.order_by(*@sort) if @sort
             @scene.rows.load
             @scene.has_more = @scene.rows.has_more?
             @scene.pages = (@scene.rows.total_rows.to_f / @attributes[:row_limit]).ceil
