14622f0925210304f3974223ce6fd276bae7c0f4
Ivan <abmajor7@gmail.com>
2009-03-24 19:38:25 +0100

    Added db functions in query order

---
+++ lib/spiderfw/model/query_funcs.rb
@@ -0,0 +1,60 @@
+module Spider; module Model
+    
+    class Query
+        
+        module Funcs
+            
+            class Function
+                attr_accessor :mapper_fields
+                
+                def self.func_name
+                    self.name =~ /::([^:]+)$/
+                    return Inflector.underscore($1).to_sym
+                end
+                
+                def func_name
+                    self.class.func_name
+                end
+                
+                def elements
+                    []
+                end
+                
+                def inner_elements
+                    els = []
+                    elements.each do |el|
+                        if (el.is_a?(Function))
+                            els += el.inner_elements
+                        else
+                            els << [el, self]
+                        end
+                    end
+                    return els
+                end
+                
+            end
+            
+            class UnaryFunction < Function
+                
+                def initialize(el)
+                    @el = el
+                end
+                
+                def elements
+                    [@el]
+                end
+                
+            end
+                        
+            class Length < UnaryFunction
+            end
+            
+            class Trim < UnaryFunction
+            end
+            
+            
+        end
+        
+    end
+    
+end; end
