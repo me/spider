3f076e6e9cab40a7aad142041b9478cfed536f31
Ivan <abmajor7@gmail.com>
2009-06-22 16:51:34 +0200

    Moved storages to config hash format; added orgs config options

--- lib/spiderfw/config/options/spider.rb
+++ lib/spiderfw/config/options/spider.rb
@@ -36,11 +36,15 @@ module Spider
         :default => Proc.new{ Spider.config.get('runmode') == 'devel' ? true : false }
     }
     
-    config_option 'storages', _("A list of named storages"), {:type => Hash}
+    config_option 'storages', _("A list of named storages"), :type => :conf
+    config_option 'storages.x.url', _("Connection url to the storage"), :type => String, :required => true
+    config_option 'storages.x.encoding', _("Encoding the DB uses"), :type => String
     
     config_option 'debugger.start', _("Start the debugger")
     
-    config_option 'locale', _("The locale to use")
+    config_option 'locale', _("The locale to use") do |val|
+        Spider.locale = val
+    end
  
     config_option 'runner.sleep', _("Sleep time for the periodic runner"), :default => 10
     
@@ -65,4 +69,31 @@ module Spider
         :choices => [false, :DEBUG, :INFO],
         :process => lambda{ |opt| opt && opt != 'false' ? opt.upcase.to_sym : false }
 #                    :process => lambda{ |v| v.upcase }
+
+
+    config_option 'orgs', _("A list of organizations"), :type => :conf
+    config_option 'orgs.x.name', _("Descriptive name of the organization")
+    config_option 'orgs.x.country_code', _("Country code of the organization")
+    config_option 'orgs.x.state', _("State of the organization")
+    config_option 'orgs.x.city', _("Name of the city")
+    config_option 'orgs.x.common_name', _("Common name (e.g. domain) of the organization")
+    config_option 'orgs.x.email', _("Main e-mail address of the organization")
+    config_option 'orgs.x.organizational_unit', _("Organizational Unit (e.g. department)")
+    config_option 'orgs.x.pub_key', _("Path to the public key (defaults to config/certs/org_name/public.pem)"),
+        :default => lambda{ |name| Spider.paths[:certs]+'/'+name+'/public.pem'}
+    config_option 'orgs.x.cert', _("Path to the certificate (defaults to config/certs/org_name/cert.pem)"),
+        :default => lambda{ |name| Spider.paths[:certs]+'/'+name+'/cert.pem'}
+    config_option 'orgs.x.private_key', _("Path to the certificate (defaults to config/certs/org_name/private/key.pem)"),
+        :default => lambda{ |name| Spider.paths[:certs]+'/'+name+'/private/key.pem'}
+
+    conf_alias 'it_IT' => {
+        'orgs' => 'organizzazioni',
+        'name' => 'nome',
+        'country_code' => 'codice_nazione',
+        'city' => 'comune',
+        'state' => 'provincia',
+        'organizational_unit' => 'unita_organizzativa'
+    }
+    
+    
 end
