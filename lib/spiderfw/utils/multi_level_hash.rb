ec2a68f729e1025549903b8490056d469beddb1f
Ivan <abmajor7@gmail.com>
2009-06-30 18:16:41 +0200

    Added HashDottedAccess module; no longer forces keys to be symbols

--- lib/spiderfw/utils/multi_level_hash.rb
+++ lib/spiderfw/utils/multi_level_hash.rb
@@ -1,11 +1,9 @@
 module Spider
     
-    class MultiLevelHash < Hash
-        
+    module HashDottedAccess
         def []=(key, val)
             parts = key.to_s.split('.', 2)
             return super(key, val) unless parts[1]
-            parts[0] = parts[0].to_sym
             self[parts[0]] ||= self.class.new
             self[parts[0]][parts[1]] = val
         end
@@ -13,10 +11,13 @@ module Spider
         def [](key)
             parts = key.to_s.split('.', 2)
             return super(key) unless parts[1]
-            parts[0] = parts[0].to_sym
             return self[parts[0]][parts[1]]
         end
+    end
         
+    
+    class MultiLevelHash < Hash
+        include HashDottedAccess
         
     end
     
