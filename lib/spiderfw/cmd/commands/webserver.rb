6cf92b4c85752034a79e628e0029652100bf5bb2
Ivan <abmajor7@gmail.com>
2009-07-13 12:33:08 +0200

    SSL options

--- lib/spiderfw/cmd/commands/webserver.rb
+++ lib/spiderfw/cmd/commands/webserver.rb
@@ -28,16 +28,30 @@ class WebServerCommand < CmdParse::Command
             opt.on("--cgi", _("Serve each request spawning a CGI subprocess. Useful in developement."), "-c"){
                 @cgi = true
             }
+            opt.on("--ssl [PORT]", _("Listen to SSL requests"), "-S"){ |port|
+                @ssl = port || 443
+            }
+            opt.on("--cert CERT", _("SSL certificate")){ |cert| @ssl_cert = cert }
+            opt.on("--key key", _("SSL private key")){ |key| @ssl_key = key }
         end
         start.set_execution_block do |args|
             require 'spiderfw'
+            raise "Can't use cgi mode with SSL" if @ssl && @cgi
             @port ||= Spider.conf.get('webserver.port')
             @server_name ||= Spider.conf.get('http.server')
             puts _("Using webserver %s") % @server_name if $verbose
             puts _("Listening on port %s") % @port if $verbose
             server = Spider::HTTP.const_get(servers[@server_name]).new
-
-            server.start(:port => @port, :cgi => @cgi)
+            thread = Thread.new do
+                server.start(:port => @port, :cgi => @cgi)
+            end
+            if (@ssl)
+                ssl_thread = Thread.new do
+                    server.start(:port => @ssl, :ssl => true)
+                end
+            end
+            thread.join
+            ssl_thread.join if ssl_thread
             
         end
         self.add_command( start )
