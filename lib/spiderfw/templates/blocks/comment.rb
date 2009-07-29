4a9aa106a3eee4525e77750943e4836757076b85
Ivan <abmajor7@gmail.com>
2009-07-29 13:15:20 +0200

    Don't parse comments

--- lib/spiderfw/templates/blocks/comment.rb
+++ lib/spiderfw/templates/blocks/comment.rb
@@ -8,11 +8,11 @@ module Spider; module TemplateBlocks
             init = nil
             c = ""
             str = ""
-            rest = scan_vars(@el.to_s) do |text, code|
-                str += escape_text(text)+"'+("+vars_to_scene(code)+").to_s+'"
-            end
-            str += escape_text(rest)
-            c += "$out << '#{str}'\n"
+            # rest = scan_vars(@el.to_s) do |text, code|
+            #     str += escape_text(text)+"'+("+vars_to_scene(code)+").to_s+'"
+            # end
+            # str += escape_text(rest)
+            c += "$out << '#{@el.to_s}'\n"
             return CompiledBlock.new(init, c)
         end
 
