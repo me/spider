28c10376b0a6245ef333503ad192060ff15f945d
Ivan <abmajor7@gmail.com>
2009-07-31 19:30:06 +0200

    Required worker; mixin; fixes

---
+++ apps/messenger/controllers/mixins/messenger_controller_mixin.rb
@@ -0,0 +1,54 @@
+require 'erb'
+require 'mailfactory'
+
+Spider.register_resource_type(:email, ['erb'], 'templates/email')
+
+module Spider; module Messenger
+    
+    module MessengerControllerMixin
+        
+        # Compiles an e-mail from given template and scene, and sends it using
+        # #Messenger::email
+        # template is the template name (found in templates/email), without the extension
+        # will use template.html.erb and template.txt.erb if they exist, template.erb otherwise.
+        # attachments must be an array of hashes like 
+        # {:file => '/full/file/path', :type => 'mime type', :file_name => 'optional email file name', 
+        # :headers => 'optional string or array of additional headers'}
+        def email(template, scene, from, to, headers={}, attachments=[], params={})
+            path_txt = self.class.find_resource(:email, template+'.txt')
+            path_txt = nil unless File.exist?(path_txt)
+            path_html = self.class.find_resource(:email, template+'.txt')
+            path_html = nil unless File.exist?(path_html)
+            scene_binding = scene.instance_eval{ binding }
+            if (path_txt || path_html)
+                text = ERB.new(IO.read(path_txt)).result(scene_binding) if path_txt
+                html = ERB.new(IO.read(path_html)).result(scene_binding) if path_html
+            else
+                path = self.class.find_resource(:email, template)
+                text = ERB.new(IO.read(path)).result(scene_binding)
+            end
+            mail = MailFactory.new
+            mail.To = to
+            mail.From = from
+            headers.each do |key, value|
+                mail.add_header(key, value)
+            end
+            mail.html = html if html
+            mail.text = text if text
+            debugger
+            if (attachments && !attachments.empty?)
+                attachments.each do |att|
+                    if (att[:file_name])
+                        mail.add_attachment_as(att[:file], att[:file_name], att[:type], att[:headers])
+                    else
+                        mail.add_attachment(att[:file], att[:type], att[:headers])
+                    end
+                end
+            end
+            mail_headers, mail_body = mail.to_s.split("\r\n\r\n")
+            Messenger.email(from, to, mail_headers, mail_body, params)
+        end
+        
+    end
+    
+end; end
--- apps/messenger/messenger.rb
+++ apps/messenger/messenger.rb
@@ -1,5 +1,6 @@
 require 'apps/messenger/models/email'
 
+
 module Spider
     
     module Messenger
@@ -35,17 +36,17 @@ module Spider
                         msg.save
                     else
                         msg.last_try = now
-                        msg.failed ||= 0
-                        msg.failed += 1
+                        msg.attempts ||= 0
+                        msg.attempts += 1
                         if (exc)
                             msg.backend_response = exc.to_s
                         else
                             msg.backend_response = res
                         end
-                        if (msg.failed >= Spider.conf.get("messenger.#{queue}.retries"))
+                        if (msg.attempts >= Spider.conf.get("messenger.#{queue}.retries"))
                             msg.next_try = nil
                         else
-                            msg.next_try = msg.last_try.to_local_time + (msg.failed * Spider.conf.get("messenger.#{queue}.retry_time") * 60)
+                            msg.next_try = msg.last_try.to_local_time + (msg.attempts * Spider.conf.get("messenger.#{queue}.retry_time") * 60)
                         end
                         msg.save
                     end
@@ -60,8 +61,8 @@ module Spider
             elsif(headers.is_a?(Array))
                 headers = headers.join("\n")
             end
-            headers = "To: #{to}\n"+headers unless headers =~ /^To/
-            headers = "From: #{from}\n"+headers unless headers =~ /^From/
+            headers = "To: #{to}\r\n"+headers unless headers =~ /^To/
+            headers = "From: #{from}\r\n"+headers unless headers =~ /^From/
             msg = Email.new(
                 :from => from, :to => to, :headers => headers, :body => body
             )
@@ -70,6 +71,7 @@ module Spider
             return msg
         end
         
+        # Actual SMTP send.
         def self.send_email(msg)
             require 'net/smtp'
             res = false
@@ -81,7 +83,7 @@ module Spider
               Spider.conf.get('messenger.smtp.password'),
               Spider.conf.get('messenger.smtp.auth_scheme')
             ) do |smtp|
-                msg_str = msg.headers+"\n"+msg.body
+                msg_str = msg.headers+"\r\n"+msg.body
                 res = smtp.send_message msg_str, msg.from, msg.to
             end
             return res.string
