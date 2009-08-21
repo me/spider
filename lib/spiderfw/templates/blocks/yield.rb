c6fe2e2418ff0baa996adccdfc7b1c5b9d3fe114
Ivan <abmajor7@gmail.com>
2009-08-21 19:59:10 +0200

    'to' attribute

--- lib/spiderfw/templates/blocks/yield.rb
+++ lib/spiderfw/templates/blocks/yield.rb
@@ -6,8 +6,9 @@ module Spider; module TemplateBlocks
         
         def compile(options={})
             init = nil
+            to = @el.attributes['to'] || 'yield_to'
             #c = "self[:yield_to][:controller].send(self[:yield_to][:action], *self[:yield_to][:arguments])\n"
-            c = "yield :yield_to\n"
+            c = "yield :#{to}\n"
             return CompiledBlock.new(init, c)
         end
         
