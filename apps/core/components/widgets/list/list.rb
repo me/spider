1f533f178d7d764a0bd2af7894f2d06ff99831f1
Ivan <ivan.pirlik@soluzionipa.it>
2009-10-07 19:47:57 +0200

    New list functionality

--- apps/core/components/widgets/list/list.rb
+++ apps/core/components/widgets/list/list.rb
@@ -9,17 +9,25 @@ module Spider; module Components
         i_attribute :keys
         is_attribute :delete, :default => false, :process => lambda{ |v| return true if v == "true"; return false if v == "false"; v }
         is_attribute :delete_param
+        is_attribute :delete_mode, :default => 'all'
         is_attribute :sortable; is_attribute :collapsable; is_attribute :collapsed
         is_attribute :tree
         is_attr_accessor :actions
         is_attribute :sub_elements
         is_attribute :is_child
         is_attribute :paginate, :type => Fixnum, :default => false
+        is_attribute :searchable, :type => Spider::DataTypes::Bool
+        attribute :show_empty, :type => Spider::DataTypes::Bool
+        i_attribute :dereference_junction
+        i_attribute :dereference_sort, :type => Spider::DataTypes::Bool, :default => false
+        i_attribute :dereference_delete, :type => Spider::DataTypes::Bool, :default => true
+        is_attribute :list_tag, :default => 'ul'
 
         def widget_init(action='')
             super
             @sortable = @sortable.to_sym if @sortable
             @tree = @tree.to_sym if @tree
+            @model = const_get_full(@model) if @model.is_a?(String)
             if (@model && !@queryset)
                 @queryset = @tree ? @model.roots : @model.all 
             elsif @queryset
@@ -29,6 +37,8 @@ module Spider; module Components
 
 
         def prepare
+            @scene.start_list_tag = "<#{@list_tag}>"
+            @scene.close_list_tag = "</#{@close_list_tag}>"
             @sublists = []
             if (@sub_elements && @sub_elements.is_a?(String))
                 @sub_elements = @sub_elements.split(/,\s+/).map{ |el| el.to_sym }
@@ -40,17 +50,43 @@ module Spider; module Components
                 @page ||= 1
                 @page = @page.to_i
                 @offset = ((@page - 1) * @attributes[:paginate])
+                @scene.page = @page
             end
-            if (!@lines)
+            @keys = []
+            @delete_keys = []
+            @values = []
+            if (params['delete'])
+                delete(params['delete'])
+            end
+            css_model = @dereference_junction ? @model.elements[@dereference_junction.to_sym].model : @model
+            @scene.model_class = css_model_class(css_model)
+            if (!@lines && @queryset)
                 @lines = []
-                @keys = []
                 cnt = 0
                 if (@attributes[:paginate])
                     @queryset.offset = @offset
                     @queryset.limit = @attributes[:paginate]
                 end
+                @scene.search_submit_text = _("Go")
+                @scene.search_clear_text = _("Clear")
+                @search_query = params['q'] || session['q']
+                @search_query = nil if params['clear']
+                session['q'] = @search_query
+                if (@attributes[:searchable] && @search_query)
+                    @scene.search_query = @search_query.to_s
+                    @queryset.condition.and(@queryset.model.free_query_condition(@search_query))
+                end
                 @queryset.each do |row|
+                    if (@dereference_junction)
+                        dr_keys = keys_string(row)
+                        row = row.get(@dereference_junction.to_sym)
+                    end
                     @keys << keys_string(row)
+                    if (@dereference_junction && !@dereference_sort)
+                        @sort_keys ||= []
+                        @sort_keys << dr_keys
+                    end
+                    @delete_keys << (@dereference_junction && !@dereference_delete) ? dr_keys : keys_string(@keys)
                     cnt2 = 0
                     if @tree && row.get(@tree).length > 0
                         sl = create_sublist("sublist_#{cnt}_#{cnt2+=1}")
@@ -60,59 +96,82 @@ module Spider; module Components
                     end
                     @sub_elements.each do |el|
                         sub = row.get(el)
-                        sl = create_sublist("sublist_#{cnt}_#{cnt2+=1}")
+                        sl = create_sublist("sublist_#{cnt}_#{el}")
                         sl.queryset = sub
                         @sublists[cnt] ||= []
                         @sublists[cnt] << sl
                     end
-                    @requested_sublists.each do |sl|
+                    @requested_sublists.each do |sbl|
+                        sl = sbl.clone
                         if (sl['element'])
                             el = @model.elements[sl['element'].to_sym]
-                            next unless el # may be ok if the query is polymorphic
-                            if (sl['tree'] && el.attributes[:reverse])
+                            #next unless el # may be ok if the query is polymorphic
+                            if (sl['tree'] && el && el.attributes[:reverse])
                                 sub = el.model.send("#{sl['tree']}_roots")
                                 sub.condition[el.attributes[:reverse]] = row
                             else
                                 sub = row.get(sl['element'].to_sym)
                             end
                         end
+                        el_name = sl['element']
                         sl.delete('element')
-                        sl = create_sublist("sublist_#{cnt}_#{cnt2+=1}", sl)
+                        sl = create_sublist("sublist_#{cnt}_#{el_name}", sl)
                         sl.queryset = sub
+                        sl.css_classes << "sublist_#{el_name}"
                         @sublists[cnt] ||= []
                         @sublists[cnt] << sl
                     end
+                    @values << row
                     @lines << format_line(row)
                     cnt += 1
                 end
+                if (@attributes[:paginate])
+                    @scene.has_more = @queryset.has_more?
+                    @scene.pages = (@queryset.total_rows.to_f / @attributes[:paginate]).ceil
+                end
             end
+            @lines ||= []
             @scene.lines = @lines
             @scene.keys = @keys
+            @scene.values = @values
             @scene.sublists = @sublists
+            @scene.delete_keys = @delete_keys
+            @scene.sort_keys = @sort_keys
             @attributes[:delete] = @request.path if @attributes[:delete] == true
-            @scene.delete_link = @attributes[:delete]
-            @scene.delete_link += "?#{@delete_param}" if (@delete_param)
+            if (@attributes[:delete])
+                @scene.delete_link = @attributes[:delete]
+                if (@delete_param)
+                    @scene.delete_link += "?#{@delete_param}"
+                else
+                    @scene.delete_link += "?_w#{param_name(self)}[delete]="
+                end
+            end
             @css_classes << 'sortable' if @sortable
             @css_classes << 'collapsable' if @collapsable
             @css_classes << 'collapsed' if @collapsed
             @css_classes << 'sublist' if @attributes[:is_child]
             @css_classes << 'tree' if @attributes[:tree]
+            @scene.show_empty = @attributes[:show_empty]
             super
         end
 
         def create_sublist(name, attributes = {})
             w = create_widget(self.class, name, @request, @response)
+
             w.attributes[:tree] = @tree
             w.attributes[:actions] = @actions
             w.attributes[:is_child] = true
             attributes.each do |key, val|
+                w.attributes[key.to_sym] = val if w.class.attribute?(key)
+            end
+            @attributes.each do |key, val|
                 w.attributes[key.to_sym] = val
             end
             return w
         end
 
         def keys_string(obj)
-            obj.class.primary_keys.map{ |pk| obj.get(pk) }.join(',')
+            obj.keys_string
         end
 
         def format_line(obj)
@@ -156,6 +215,19 @@ module Spider; module Components
             end
             obj.save
             $out << "{res: 'ok'}"
+        end
+        
+        def delete(id)
+            if (@delete_mode == 'all')
+                obj = @model.new(id)
+                if (@tree)
+                    obj.mapper.tree_delete(@model.elements[@tree], obj)
+                else
+                    obj.delete
+                end
+            else
+                
+            end
         end
 
 
