7a6889b25d3c0dc2ae2de37c7946d4ada33e6b68
Ivan <ivan.pirlik@soluzionipa.it>
2009-10-12 19:30:34 +0200

    <!--- and <!--+ comments

--- lib/spiderfw/templates/blocks/comment.rb
+++ lib/spiderfw/templates/blocks/comment.rb
@@ -7,12 +7,27 @@ module Spider; module TemplateBlocks
         def compile(options={})
             init = nil
             c = ""
-            str = ""
-            rest = scan_vars(@el.to_s) do |text, code|
-                str += escape_text(text)+"'+("+vars_to_scene(code)+").to_s+'"
+            str = nil
+            mode = nil
+            if (@el.content[0].chr == '-')
+                mode = :internal
+            elsif (@el.content[0].chr == '+')
+                mode = :parse
+            else
+                mode = :verbatim
             end
-            str += escape_text(rest)
-            c += "$out << '#{str}'\n"
+            if (mode == :parse)
+                str = ""
+                rest = scan_vars(@el.to_s) do |text, code|
+                    str += escape_text(text)+"'+("+vars_to_scene(code)+").to_s+'"
+                end
+                str += escape_text(rest)
+            elsif (mode == :verbatim)
+                str = escape_text(@el.to_s)
+            else
+                str = nil
+            end
+            c += "$out << '#{str}'\n" if str
             #c += "$out << '#{@el.to_s}'\n"
             return CompiledBlock.new(init, c)
         end
