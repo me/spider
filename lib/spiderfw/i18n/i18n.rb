703bcf1ec624735743f3be73fb529cd8d0e36cd5
Ivan <abmajor7@gmail.com>
2009-10-14 15:46:06 +0200

    Finished distance_of_time_in_words

--- lib/spiderfw/i18n/i18n.rb
+++ lib/spiderfw/i18n/i18n.rb
@@ -78,40 +78,36 @@ module Spider
                 parse_dt(locale, string, format, options.merge({:return => :datetime}))
             end
             
-            # from Rails!
-            # TODO: finish
-            def distance_of_time_in_words(locale, from_time, to_time = 0, include_seconds = false)
-                begin
-                    provider = Rails.new(locale)
-                rescue => exc
-                    return ""
-                end
+            # from Rails
+            def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false)
                 from_time = from_time.to_time if from_time.respond_to?(:to_time)
+                from_time = from_time.to_local_time if from_time.respond_to?(:to_local_time)
                 to_time = to_time.to_time if to_time.respond_to?(:to_time)
+                to_time = to_time.to_local_time if to_time.respond_to?(:to_local_time)
                 distance_in_minutes = (((to_time - from_time).abs)/60).round
                 distance_in_seconds = ((to_time - from_time).abs).round
 
                 case distance_in_minutes
                 when 0..1
-                    return (distance_in_minutes == 0) ? 'less than a minute' : '1 minute' unless include_seconds
+                    return (distance_in_minutes == 0) ? _('less than a minute') : _('1 minute') unless include_seconds
                     case distance_in_seconds
-                    when 0..4   then 'less than 5 seconds'
-                    when 5..9   then 'less than 10 seconds'
-                    when 10..19 then 'less than 20 seconds'
-                    when 20..39 then 'half a minute'
-                    when 40..59 then 'less than a minute'
-                    else             '1 minute'
+                    when 0..4   then _('less than %s seconds') % '5'
+                    when 5..9   then _('less than %s seconds') % '10'
+                    when 10..19 then _('less than %s seconds') % '20'
+                    when 20..39 then _('half a minute')
+                    when 40..59 then _('less than a minute')
+                    else             _('1 minute')
                     end
 
-                when 2..44           then "#{distance_in_minutes} minutes"
-                when 45..89          then 'about 1 hour'
-                when 90..1439        then "about #{(distance_in_minutes.to_f / 60.0).round} hours"
-                when 1440..2879      then '1 day'
-                when 2880..43199     then "#{(distance_in_minutes / 1440).round} days"
-                when 43200..86399    then 'about 1 month'
-                when 86400..525599   then "#{(distance_in_minutes / 43200).round} months"
-                when 525600..1051199 then 'about 1 year'
-                else                      "over #{(distance_in_minutes / 525600).round} years"
+                when 2..44           then _("%s minutes") % distance_in_minutes
+                when 45..89          then _('about 1 hour')
+                when 90..1439        then _("about %d hours") % (distance_in_minutes.to_f / 60.0).round
+                when 1440..2879      then _('1 day')
+                when 2880..43199     then _("%d days") % (distance_in_minutes / 1440).round
+                when 43200..86399    then _('about 1 month')
+                when 86400..525599   then _("%d months") % (distance_in_minutes / 43200).round
+                when 525600..1051199 then _('about 1 year')
+                else                      _("over %d years") % (distance_in_minutes / 525600).round
                 end
             end
             
