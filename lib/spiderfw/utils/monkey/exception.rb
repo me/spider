5229737d9b7de996e24bd84421b703ef6e9267e2
Ivan <abmajor7@gmail.com>
2009-10-27 14:41:44 +0100

    UUID in exceptions; fixed exception logging

---
+++ lib/spiderfw/utils/monkey/exception.rb
@@ -0,0 +1,23 @@
+require 'uuid'
+
+module UUIDException
+    
+    def uuid=(val)
+        @uuid = val
+    end
+    
+    def uuid
+        @uuid
+    end
+    
+end
+
+module UUIDExceptionMessage
+    def message
+        "#{@uuid} - #{super}"
+    end
+end
+
+class Exception
+    include UUIDException
+end
