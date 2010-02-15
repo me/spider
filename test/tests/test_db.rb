f02f614a71c991b0436a721ed6db2815a8e5a684
Ivan <ivan.pirlik@soluzionipa.it>
2010-02-15 15:52:01 +0100

    More pool tests

--- test/tests/test_db.rb
+++ test/tests/test_db.rb
@@ -20,20 +20,66 @@ class TestDb < Spider::TestCase
         assert_equal(0, pool.size)
         
         threads = []
-        1.upto(pool.max_size*2) do |i|
-            threads[i] = Thread.new do
+        limit = 100
+        while (pool.size < pool.max_size && threads.length < limit)
+            Spider.logger.debug "POOL SIZE: #{pool.size}"
+            threads << Thread.new do
                 1.upto(100) do |j|
                     Zoo::Animal.all.load
                 end
             end
         end
-        pool = Zoo::Animal.storage.connection_pool
-        sleep(1)
+        Spider.logger.debug "THREADS: #{threads.length}"
+        
+        assert(threads.length < limit)
+        
+      #  sleep(1)
 
-        assert_equal(pool.max_size, pool.size)
+     #   assert_equal(pool.max_size, pool.size)
 
-        threads.each{ |t| t.kill if t }
+        threads.each{ |t| t.join  }
         pool.clear
+
+        #pool.timeout = pool.max_size
+        
+        threads = []
+        1.upto(pool.max_size * 2) do |i|
+            threads << Thread.new do
+                conn = pool.get_connection
+                sleep(1)
+                pool.release(conn)
+            end
+        end
+        
+        threads.each{ |t| t.join  }
+        pool.clear
+
+        
+        Zoo::Food.static(:id => 'test_food1', :name => 'Test1').insert
+        Zoo::Food.static(:id => 'test_food2', :name => 'Test2').insert
+        threads = []
+        i = 0
+        # This should build up some queue
+        while threads.length < pool.max_size * 3
+            i += 1
+            threads << Thread.new do
+                1.upto(50) do |j|
+                    animal = Zoo::Animal.static(:id => "test_animal#{Thread.current}", :name => 'Test')
+                    animal.foods << 'test_food1'; animal.foods << 'test_food2'
+                    animal.insert
+                    animal = Zoo::Animal.new("test_animal#{Thread.current}")
+                    animal.delete
+                end
+            end
+        end
+        
+        threads.each{ |t| t.join  }        
+        
+        assert_equal(pool.max_size, pool.size)
+        assert_equal(0, Zoo::Animal.where(:name => 'Test').total_rows)
+        assert_equal(0, Zoo::Animal::Foods.where('food.id' => 'test_food1').total_rows)
+        
+
 
     end
     
