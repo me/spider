ba49f5744c1b28a348fe2bf891c99466cd6d922b
Ivan <ivan.pirlik@soluzionipa.it>
2010-03-24 16:30:46 +0100

    Added superuser labels

--- apps/core/auth/models/super_user.rb
+++ apps/core/auth/models/super_user.rb
@@ -2,6 +2,7 @@ module Spider; module Auth
     
     class SuperUser < LoginUser
         extend_model superclass, :add_polymorphic => true
+        label _('Superuser'), _('Superusers')
         include LoginAuthenticator
     end
     
