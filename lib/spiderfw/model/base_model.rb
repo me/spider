78c10f8eedd5b351e7b7310848ce461065a98b4e
Ivan <ivan.pirlik@soluzionipa.it>
2010-04-14 13:01:24 +0200

    Raise error if initialize fails

--- lib/spiderfw/model/base_model.rb
+++ lib/spiderfw/model/base_model.rb
@@ -1036,6 +1036,8 @@ module Spider; module Model
                  # Single unset key, single value
                 elsif ((empty_keys = self.class.primary_keys.select{ |key| !element_has_value?(key) }).length == 1)
                     set(empty_keys[0], values)
+                else
+                    raise ArgumentError, "Don't know how to construct a #{self.class} from #{values.inspect}"
                 end
             end
         end
