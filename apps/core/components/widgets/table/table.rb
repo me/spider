45c0eeae4a05298544bd6afb101b9e3e57ded3cb
Ivan <ivan.pirlik@soluzionipa.it>
2010-04-15 15:43:40 +0200

    Number formatting in Table widget

--- apps/core/components/widgets/table/table.rb
+++ apps/core/components/widgets/table/table.rb
@@ -126,7 +126,9 @@ module Spider; module Components
                         if (!row[el])
                             res_row[el] = '' 
                         elsif (element.type <= Date || element.type <= Time)
-                            res_row[el] = Spider::I18n.localize(@request.locale, row[el], :short)
+                            res_row[el] = Spider::I18n.localize_date_time(@request.locale, row[el], :short)
+                        elsif (element.type <= Float || element.type <= BigDecimal)
+                            res_row[el] = Spider::I18n.localize_number(@request.locale, row[el])
                         elsif (row[el].respond_to?(:format))
                             res_row[el] = row[el].format(:short)
                         else                            
