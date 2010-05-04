2b61282a82f0d04fe5cd67a7d4c6ced55332f7af
Ivan <abmajor7@gmail.com>
2010-05-04 21:10:45 +0200

    Cleaned up blueprint _init.rb

--- blueprints/app/_init.rb
+++ blueprints/app/_init.rb
@@ -1,10 +1,7 @@
-require 'apps/<%= app_path %>/controllers/<%= app_name %>_controller.rb'<% tab = (modules.length > 1) ? "    " : "" %><%= "\n" if (modules.length > 1) %>
-<%= modules[0..-2].inject(""){ |s, mod| s+= "; " if (s.length > 0); s+="module #{mod}"}+"\n" if (modules.length > 1)%>
-<%=tab%>module <%=modules[-1]%>
-<%=tab%>    @description = ""
-<%=tab%>    @version = 0.1
-<%=tab%>    @path = File.dirname(__FILE__)
-<%=tab%>    @controller = :<%= module_name %>Controller
+<%= modules[0..-2].inject(""){ |s, mod| s+= "; " if (s.length > 0); s+="module #{mod}"}+"\n" if (modules.length > 1)%><%= "\n" if (modules.length > 1) %><% tab = (modules.length > 1) ? "    " : "" 
+%><%=tab%>module <%=modules[-1]%>
 <%=tab%>    include Spider::App
+<%=tab%>    @controller = :<%= module_name %>Controller
 <%=tab%>end
-<%= "\n"+modules[0..-2].inject(""){ |s, mod| s+= "; " if (s.length > 0); s+="end" if (modules.length > 1)} %>
+<%= "\n"+modules[0..-2].inject(""){ |s, mod| s+= "; " if (s.length > 0); s+="end" if (modules.length > 1)} 
+%>require 'apps/<%= app_path %>/controllers/<%= app_name %>_controller'
