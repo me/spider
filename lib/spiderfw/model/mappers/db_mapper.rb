fc0a912b1cd65ce9725ba23b0169381a8d127525
Ivan <ivan.pirlik@soluzionipa.it>
2010-05-11 19:39:29 +0200

    Fixed error if condition key is a QueryFunc

--- lib/spiderfw/model/mappers/db_mapper.rb
+++ lib/spiderfw/model/mappers/db_mapper.rb
@@ -530,7 +530,8 @@ module Spider; module Model; module Mappers
                 element = model.elements[k.to_sym]
                 next unless model.mapper.mapped?(element)
                 if (element.model?)
-                    if (v && model.mapper.have_references?(element.name) && v.select{ |key, value| !element.model.elements[key].primary_key? }.empty?)
+                    if (v && model.mapper.have_references?(element.name) && v.select{ |key, value| 
+                        !key.is_a?(Element) || !element.model.elements[key].primary_key? }.empty?)
                         # 1/n <-> 1 with only primary keys
                         element_cond = {:conj => 'AND', :values => []}
                         v.each_with_comparison do |el_k, el_v, el_comp|
