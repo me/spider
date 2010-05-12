c04729af6a3b1eecd20d73d95407896c26c8f387
Ivan <ivan.pirlik@soluzionipa.it>
2010-05-12 12:53:58 +0200

    Fixed condition for direct values

--- lib/spiderfw/model/mappers/db_mapper.rb
+++ lib/spiderfw/model/mappers/db_mapper.rb
@@ -531,7 +531,7 @@ module Spider; module Model; module Mappers
                 next unless model.mapper.mapped?(element)
                 if (element.model?)
                     if (v && model.mapper.have_references?(element.name) && v.select{ |key, value| 
-                        !key.is_a?(Element) || !element.model.elements[key].primary_key? }.empty?)
+                        key.is_a?(QueryFuncs::Function) || !element.model.elements[key].primary_key? }.empty?)
                         # 1/n <-> 1 with only primary keys
                         element_cond = {:conj => 'AND', :values => []}
                         v.each_with_comparison do |el_k, el_v, el_comp|
