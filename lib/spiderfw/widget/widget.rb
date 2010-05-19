53781582e10bbe81a7d7fd9e63508df394b8e9ed
Ivan <ivan.pirlik@soluzionipa.it>
2010-05-19 11:09:43 +0200

    Changed widget js includes

--- lib/spiderfw/widget/widget.rb
+++ lib/spiderfw/widget/widget.rb
@@ -209,9 +209,14 @@ module Spider
             locale = @request.locale.language
             include_js = [
                 '/js/jquery/jquery-1.4.2.js', '/js/inheritance.js', '/js/spider.js', '/js/jquery/plugins/jquery.query-2.1.6.js',
-                '/js/jquery/jquery-ui/development-bundle/ui/jquery-ui-1.7.2.custom.js', #'/js/jquery/jquery-ui/development-bundle/ui/jquery-ui-1.7.2.custom.min.js'
-                "/js/jquery/jquery-ui/development-bundle/ui/i18n/ui.datepicker-#{locale}.js", '/js/jquery/plugins/jquery.form.js'
+                '/js/jquery/plugins/jquery.form.js',
+                '/js/plugins/plugin.js'
             ]
+            # include_js << [
+            #     '/js/jquery/jquery-ui/development-bundle/ui/jquery-ui-1.7.2.custom.js',
+            #     #'/js/jquery/jquery-ui/development-bundle/ui/jquery-ui-1.7.2.custom.min.js',
+            #     "/js/jquery/jquery-ui/development-bundle/ui/i18n/ui.datepicker-#{locale}.js"
+            # ]
             include_css = [
                 '/css/spider.css', '/js/jquery/jquery-ui/css/smoothness/jquery-ui-1.7.2.custom.css', 
             ]
