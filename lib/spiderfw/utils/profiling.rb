e6d9f0a1c61deed8bd24c8730d067c5765ac2b1b
Ivan <ivan.pirlik@soluzionipa.it>
2010-08-25 16:16:05 +0200

    Profiling util

---
+++ lib/spiderfw/utils/profiling.rb
@@ -0,0 +1,36 @@
+module Spider
+    
+    module Profiling
+        
+        def self.start
+            raise "Profiling already started" if @profiling_started
+            unless Spider.request_mutex
+                Spider.mutex_requests!
+                Spider.request_mutex.lock
+            end
+            require 'ruby-prof'
+            Spider.logger.debug("Starting profiling")
+            @profiling_started = true
+            RubyProf.start
+        end
+        
+        def self.stop
+            Spider.request_mutex.unlock
+            Spider.request_mutex = nil
+            result = RubyProf.stop
+            @profiling_started = false
+            printer = ::RubyProf::GraphHtmlPrinter.new(result)
+            file_name = Spider.paths[:tmp]+"/prof_#{DateTime.now.to_s}.html"
+            File.open(file_name, 'w') do |f|
+                printer.print(f, :min_percent => 0)
+            end
+            Spider.logger.info("Written profiling info in #{file_name}")
+        end
+        
+        def self.started?
+            @profiling_started
+        end
+        
+    end
+    
+end
