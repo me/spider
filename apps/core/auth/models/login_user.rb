268e05ff911d9a60b917de0001b423898ffc1b2f
Ivan <ivan.pirlik@soluzionipa.it>
2010-08-25 14:42:01 +0200

    Added username checks

--- apps/core/auth/models/login_user.rb
+++ apps/core/auth/models/login_user.rb
@@ -3,12 +3,22 @@ module Spider; module Auth
     class LoginUser < User
         extend_model superclass, :add_polymorphic => true
         include LoginAuthenticator
-        element :username, String, :required => true, :unique => true
+        element :username, String, :required => true, :unique => true, :check => {
+            _("The username contains disallowed characters") => /^[^\x00-\x1f]+$/
+        }
+        # TODO: activate
+        # element :simplified_username, String, :default => lambda{ |obj| obj.class.simplify_username(obj.username) }
         element :password, Password
 
         def identifier
             username
         end
+        
+        # TODO: activate
+        # def self.simplify_username(name)
+        #     return "" unless name
+        #     name.gsub(/[^a-z0-9_]/, '').downcase
+        # end
         
     end
     
