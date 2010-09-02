4c260bac8101c381a5c2329f4a5a0a5ab49559ff
Ivan <ivan.pirlik@soluzionipa.it>
2010-09-02 17:53:44 +0200

    Global Managed.no_set_dates method

--- lib/spiderfw/model/extended_models/managed.rb
+++ lib/spiderfw/model/extended_models/managed.rb
@@ -30,9 +30,24 @@ module Spider; module Model
             true
         end
         
+        def self.no_set_dates=(bool)
+            @no_set_dates = bool
+        end
+        
+        def self.no_set_dates
+            @no_set_dates
+        end
+        
         def _set_dates(bool=nil)
+            return false if Spider::Model::Managed.no_set_dates
             @_set_dates = true if @_set_dates == nil
             @_set_dates = bool if bool != nil
+            if self.class.extended_models
+                self.class.extended_models.each do |mod, el|
+                    sup = self.get(el)
+                    sup._set_dates(bool) if sup
+                end
+            end
             @_set_dates
         end
         
