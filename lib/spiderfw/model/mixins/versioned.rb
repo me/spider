1ab4a277f6e88bc1f197704d50bf984ecc148696
Ivan <ivan.pirlik@soluzionipa.it>
2010-09-10 19:59:51 +0200

    Various Versioned fixes

--- lib/spiderfw/model/mixins/versioned.rb
+++ lib/spiderfw/model/mixins/versioned.rb
@@ -5,6 +5,7 @@ module Spider; module Model
     
     module Versioned
         
+        
         def self.included(model)
             model.extend(ClassMethods)
             model.mapper_include(Mapper)
@@ -51,7 +52,7 @@ module Spider; module Model
                   @version_models[branch] = vmod
               end
               vmod = @version_models[branch]
-              if self.attributes[:sub_model]
+              if self.attributes[:sub_model] && self.attributes[:sub_model].respond_to?(:version_model)
                   vmod.attributes[:sub_model] = self.attributes[:sub_model].version_model
               end
               if @polymorphic_models
@@ -69,21 +70,30 @@ module Spider; module Model
               end
               # vmod.remove_element(:v_sha1)
               self.remove_element(:history) # may exist from superclass
-              vmod.element(:v_original, self, :add_multiple_reverse => {:name => :history, :version_history => true})
+              vmod.element(:v_original, self, :add_multiple_reverse => {:name => :history, :version_history => true}, :hidden => true)
               vmod.element(:version_date, DateTime)
               vmod.element(:version_comment, String)
+              vmod.remove_element(:history)
               self.elements_array.each do |el|
                   next if el.name == :v_sha1
                   elh = dump_element(el)
-                  next if el.integrated?
+                  if el.integrated?
+                      if el.integrated_from.type < Versioned && vmod.elements[el.name] && vmod.elements[el.name].model? && \
+                          !vmod.elements[el.name].integrated_from.type.attributes[:version_model]
+                          vmod.remove_element(el) 
+                      end
+                      next
+                  end
                   next unless elh
                   # elh[:attributes][:primary_key] = false
                   # debugger if elh[:attributes][:junction]
                   # vmod.send(elh[:method], elh[:name], elh[:type], elh[:attributes])
                   if (el.model?)
                       if elh[:attributes][:integrated_model]
+                          
                           elh[:type].elements.each do |ielname, iel|
-                              vmod.remove_element(ielname) if vmod.elements[ielname] && vmod.elements[ielname].integrated?
+                              i = vmod.elements[ielname]
+                              vmod.remove_element(ielname) if i && i.integrated? && i.integrated_from.name == el.name
                           end
                           elh[:attributes][:integrate] = true
                       end
@@ -91,7 +101,16 @@ module Spider; module Model
                           rev = el.attributes[:add_reverse] || el.attributes[:add_multiple_reverse]
                           el.type.version_model.remove_element(rev[:name]) if el.type.respond_to?(:version_model)
                       end
-                      if (!el.attributes[:added_reverse] &&  el.model < Spider::Model::Versioned)
+                      if vmod.elements[el.name] && elh[:attributes][:reverse] && !(el.model < Spider::Model::Versioned)
+                          vmod.elements[el.name].attributes.delete(:reverse)
+                      end
+                      if el.multiple? && !el.attributes[:junction] && !(el.model < Spider::Model::Versioned)
+                          elh[:attributes].delete(:reverse)
+                          elh[:attributes].delete(:add_reverse)
+                          elh[:attributes].delete(:add_multiple_reverse)
+                          vmod.remove_element(el.name)
+                          vmod.send(elh[:method], el.name, el.type, elh[:attributes])
+                      elsif (!el.attributes[:added_reverse] &&  el.model < Spider::Model::Versioned)
                           vmod.remove_element(el.name)
                           if elh[:method] == :tree
                               vmod.remove_element(elh[:attributes][:reverse])
@@ -103,14 +122,9 @@ module Spider; module Model
                               vmod.send(elh[:method], el.name, el.type.version_model, elh[:attributes])
                           end
                           @version_elements[el.name] = el.name
-                      elsif (el.attributes[:junction] && el.type < Spider::Model::Versioned)
-                          
-                          # el.attributes[:association_type].module_eval{ include Spider::Model::Versioned }
-                          # el.attributes[:association_type].versioning(branch)
-                          # elh[:attributes].
-                          # debugger
+                      elsif (el.attributes[:junction] && el.attributes[:owned])
                           # elh[:attributes][:has_single_reverse] = (el.attributes[:reverse] && !el.type.elements[el.attributes[:reverse]].multiple?)
-                          junction = el.attributes[:association_type]
+                          junction = vmod.elements[el.name].attributes[:association_type]
                           unless junction < Spider::Model::Versioned 
                               junction.module_eval{ include Spider::Model::Versioned } 
                               junction.versioning(branch)
@@ -120,7 +134,18 @@ module Spider; module Model
                               elh[:attributes][:junction_our_element] = "#{elh[:attributes][:reverse]}".to_sym
                               elh[:attributes][:junction_their_element] = "#{elh[:attributes][:junction_their_element]}".to_sym
                               vmod.remove_element(el.name)
-                              vmod.send(elh[:method], el.name, el.type.version_model, elh[:attributes])
+                              junction_type = nil
+                              if el.type.respond_to?(:version_model)
+                                  junction_type = el.type.version_model
+                              else
+                                  junction_type = el.type
+                                  elh[:attributes].delete(:reverse)
+                                  elh[:attributes].delete(:add_reverse)
+                                  elh[:attributes].delete(:add_multiple_reverse)
+                              end
+                              vmod.send(elh[:method], el.name, junction_type, elh[:attributes])
+                              
+                              junction.version_model.integrate(elh[:attributes][:junction_their_element]) if junction.attributes[:sub_model] == self
                           end
                           @version_elements[el.name] = el.name
                       end
@@ -149,7 +174,7 @@ module Spider; module Model
 
                self.elements_array.select{ |el|
                    el.attributes[:version_content] || mapper.have_references?(el.name) || (!el.attributes[:added_reverse] && !no_content_assocs.include?(el.association))
-               }
+               }.reject{ |el| el.model? && !(el.model < Spider::Model::Versioned) }
            end
            
            def version_ignored_elements
@@ -363,12 +388,25 @@ module Spider; module Model
                             else
                                 vobj.set(el.name, el_val)
                             end
+                        elsif !(el.model < Versioned) && !el.multiple? #!(el.model < Versioned && !el.multiple?)
+                            #debugger unless vobj.class.elements[el.name].model < Versioned
+                            vobj.set(el.name, self.get(el))
+                        # elsif !(el.type < Versioned)
+                        #                             el_val = self.get(el)
+                        #                             if el.multiple?
+                        #                                 el_val.each{ |el_v| vobj.get(el.name) << el_v }
+                        #                             else
+                        #                                 
+                        #                                 vobj.set(el.name, el_val)
+                        #                                 
+                        #                             end
                         end
                     else
                         el_val = self.get(el)
                         vobj.set(el.name, el_val)
                     end
                 end
+                # debugger
 
                 current_sha1 = self.get(ve)
                 new_sha1 = self.version_sha1
@@ -379,10 +417,14 @@ module Spider; module Model
                 # @@already_saved[self.class][self.primary_keys] = self.version_flatout
                 self.set(ve, new_sha1)
                 self.set_modified(ve => true)
+                # debugger unless self.primary_keys_set?
                 im.put(self)
                 self.mapper.do_update(self)
-            
-                vobj.set(:v_sha1, new_sha1)
+                
+                Spider::Model.no_identity_mapper do
+                    vobj.set(:v_sha1, new_sha1)
+                    vobj = im.put(vobj, true)
+                end
             
                 vobj.set(:version_date, DateTime.now)
                 vobj.set(:version_comment, comment)
@@ -397,6 +439,8 @@ module Spider; module Model
                 update_version_contents(vobj)
                 
                 @__version_saved = true
+                
+                trigger(:version_saved)
             
             end
 
@@ -409,15 +453,19 @@ module Spider; module Model
             self.class.elements_array.each do |el|
 
                 next unless vmod.elements[el.name]
-                next if el.model? && el.type.attributes[:version_model]
                 next if el.integrated?
+                next unless el.model?
+                next if el.type.attributes[:version_model]
+                
+                next unless vmod.elements[el.name].model < Spider::Model::Versioned
+                
                 # vobj.set(el.name, self.get(el))
                 # vel = mod.version_element(el)
                 if el.model?
                     if mod.mapper.have_references?(el)
 
                     end
-                    if !mod.mapper.have_references?(el) && el.reverse
+                    if !vmod.mapper.have_references?(el) && el.reverse
                         # cond = Spider::Model::Condition.and
                         # cond[el.reverse] = self
                         # ass = el.model.where(cond)
@@ -431,20 +479,46 @@ module Spider; module Model
                         # end
                         
                         ass = self.get(el)
+                        # debugger if el.junction?
+                        ass = self.instance_variable_get("@#{el.name}_junction") if el.junction? && !el.attributes[:keep_junction]
+                        next unless ass
                         if self.respond_to?(:prepare_version_object)
                             ass = self.prepare_version_object(el, ass)
                         end
                         ass = [ass] unless ass.is_a?(Enumerable)
                         ass.each do |row|
-                            row.save_version
+                            
+                            # if el.junction? && !row.class.attributes[:version_model]
+                            #     debugger
+                            #     jobj = el.model.new
+                            #     jobj.set(el.attributes[:junction_their_element], row)
+                            #     jobj.set(el.attributes[:reverse], self)
+                            #     jobj.save_version
+                            # else
+                                row.save_version
+                           # end
                         end
                         
                     end
-                else
-                    el_val = self.get(el)
-                    vobj.set(el.name, el_val)
                 end
             end
+            self.class.elements_array.each do |el|
+                vel = vmod.elements[el.name]
+                next unless vel
+                if el.multiple? && !(vel.model < Spider::Model::Versioned)
+                    # debugger
+                    
+                    self.get(el).each do |row|
+                        junction = vel.model.new
+                        junction.set(vel.attributes[:reverse], vobj)
+                        junction.set(vel.attributes[:junction_their_element], self)
+                        junction.insert
+                    end
+                end
+            end
+            # debugger if do_save
+            # vobj.update if do_save
+            vobj
         end
 
         
@@ -463,6 +537,18 @@ module Spider; module Model
             el = self.class.polymorphic_models[model][:through]
             super_obj = obj.get(el)
             return model.where(el => super_obj).order_by(:version_date, :desc).limit(1)[0]
+        end
+        
+        def previous_version(back=1)
+            self.class.version_model.where{ |s| 
+                (s.v_original == self) & (s.v_sha1 .not self.v_sha1) 
+            }.order_by(:version_date, :desc).limit(1).offset(back-1)[0]
+        end
+        
+        def last_version
+            self.class.version_model.where{ |s| 
+                (s.v_original == self)
+            }.order_by(:version_date, :desc).limit(1)[0]
         end
         
         
