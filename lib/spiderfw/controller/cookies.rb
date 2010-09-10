167c15296a4d911268f936260eab16981a0b5452
Ivan <ivan.pirlik@soluzionipa.it>
2010-09-10 19:54:43 +0200

    Removed excessive debug

--- lib/spiderfw/controller/cookies.rb
+++ lib/spiderfw/controller/cookies.rb
@@ -5,10 +5,7 @@ module Spider
     class Cookies < Hash
         
         def []=(key, val)
-            Spider::Logger.debug("*********************************")
-            Spider::Logger.debug("Creating cookie #{key}, #{val}")
             super(key.to_s, Cookie.new(val))
-            Spider::Logger.debug(self)
         end
         
     end
