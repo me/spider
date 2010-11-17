16304f8e63d41c55a064f8cb7cd952f359ef4c4b
Ivan <ivan.pirlik@soluzionipa.it>
2010-11-17 12:05:31 +0100

    app_server config options

--- apps/app_server/config/options.rb
+++ apps/app_server/config/options.rb
@@ -1,2 +1,6 @@
+Spider.config_option 'app_server.url', _("Url of the app server"), :default => 'http://www.soluzionipa.it/euroservizi/spider/app_server'
 Spider.config_option 'app_server.search_paths', _("Paths to search for apps"), :type => Array
+Spider.config_option 'app_server.git_ssh', _("Name of the ssh server")
+Spider.config_option 'app_server.git_http', _("Http git prefix for each git path"), :type => Hash
+
 
