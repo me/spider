30aa58e2b56d2477cc04052858eb1fc076b4dcc1
Ivan <ivan.pirlik@soluzionipa.it>
2010-11-26 18:36:24 +0100

    Added http_client util

---
+++ lib/spiderfw/utils/http_client.rb
@@ -0,0 +1,16 @@
+module Spider
+
+    def self.http_client
+        uri = URI.parse('http://www.test.com')
+        proxy = uri.find_proxy
+        klass = nil
+        if proxy
+            proxy_user, proxy_pass = nil
+            proxy_user, proxy_pass = proxy.userinfo.split(/:/) if proxy.userinfo
+            Net::HTTP::Proxy(proxy.host, proxy.port, proxy_user, proxy_pass)
+        else
+            Net::HTTP
+        end
+    end
+
+end
