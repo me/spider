d4766cba6cf4a90ea77710406fb262eb5e4bb436
Ivan <ivan.pirlik@soluzionipa.it>
2011-07-08 12:28:29 +0200

    Added app_server git_repo config settings

--- apps/app_server/config/options.rb
+++ apps/app_server/config/options.rb
@@ -2,5 +2,5 @@ Spider.config_option 'app_server.url', _("Url of the app server"), :default => '
 Spider.config_option 'app_server.search_paths', _("Paths to search for apps"), :type => Array
 Spider.config_option 'app_server.git_ssh', _("Name of the ssh server")
 Spider.config_option 'app_server.git_http', _("Http git prefix for each git path"), :type => Hash
-
-
+Spider.config_option 'app_server.git_repo_base', _("Base path for read-only git repository access")
+Spider.config_option 'app_server.git_repo_rw_base', _("Base path for read-write git repository access")
