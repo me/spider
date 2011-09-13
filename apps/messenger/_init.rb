782b01f621beabd9189e494a67d1618a2ecd216b
Ivan <ivan.pirlik@soluzionipa.it>
2011-09-13 20:11:26 +0200

    Avoid errors if messenger.*.backends is not set (i.e. init_base not done)

--- apps/messenger/_init.rb
+++ apps/messenger/_init.rb
@@ -21,9 +21,9 @@ require 'apps/messenger/messenger'
 require 'apps/messenger/controllers/messenger_controller'
 require 'apps/messenger/controllers/mixins/messenger_helper'
 
-Spider.conf.get('messenger.email.backends').each do |backend|
+(Spider.conf.get('messenger.email.backends') || []).each do |backend|
     require File.join('apps/messenger/backends/email/', backend)
 end
-Spider.conf.get('messenger.sms.backends').each do |backend|
+(Spider.conf.get('messenger.sms.backends') || []).each do |backend|
     require File.join('apps/messenger/backends/sms/', backend)
 end
