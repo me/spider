c54c7aa014214c40040901fa5a1e5531c3ba8cce
Ivan <ivan.pirlik@soluzionipa.it>
2011-09-29 17:54:22 +0200

    Preprocess integrated subconditions

--- lib/spiderfw/model/mappers/mapper.rb
+++ lib/spiderfw/model/mappers/mapper.rb
@@ -951,7 +951,9 @@ module Spider; module Model
                         condition.delete(k)
                         integrated_from = element.integrated_from
                         integrated_from_element = element.integrated_from_element
-                        condition.set("#{integrated_from.name}.#{integrated_from_element}", c, v)
+                        sub = condition.get_deep_obj
+                        sub.set(integrated_from_element, c, v)
+                        condition[integrated_from.name] = integrated_from.model.mapper.preprocess_condition(sub) 
                     elsif element.junction? && !v.is_a?(BaseModel) && !v.is_a?(Hash) && !v.nil? # conditions on junction id don't make sense
                         condition.delete(k)
                         condition.set("#{k}.#{element.attributes[:junction_their_element]}", c, v)
