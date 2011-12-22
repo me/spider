57c5d5a30f1f0e55b1dec29cef23eca635f9fccc
Ivan <ivan.pirlik@soluzionipa.it>
2011-12-22 20:04:01 +0100

    Better messenger admin

--- apps/messenger/controllers/messenger_admin_controller.rb
+++ apps/messenger/controllers/messenger_admin_controller.rb
@@ -26,25 +26,40 @@ module Spider; module Messenger
             @scene.queues.each do |name|
                 details = Spider::Messenger.queues[name]
                 model = Spider::Messenger.const_get(details[:model])
-                @scene.queue_info[name] = {
+                @scene.queue_info[name].merge!({
                     :sent => model.sent_messages.total_rows,
                     :queued => model.queued_messages.total_rows,
                     :failed => model.failed_messages.total_rows
-                }
+                })
             end
 
         end
 
-        __.html :template => 'admin/queue'
-        def queue
+        __.html
+        def queue(id=nil)
             q = Messenger.queues[@queue]
             model = Spider::Messenger.const_get(q[:model])
             @scene.queue = @queue
             @scene.title = q[:label]
             @scene.admin_breadcrumb << {:label => @scene.title, :url => self.class.url(@queue)}
+
+            if id
+                return view_message(@queue, id)
+            end
+            @scene.msg_view_url = self.class.url(@queue)+'/'
             @scene.queued = model.queued_messages
             @scene.sent = model.sent_messages
             @scene.failed = model.failed_messages
+            render 'admin/queue'
+        end
+
+        def view_message(queue, id)
+            details = Spider::Messenger.queues[queue]
+            model = Spider::Messenger.const_get(details[:model])
+            @scene.message = model.load(:id => id)
+            raise NotFound.new("Message #{queue} #{id}") unless @scene.message
+            @scene.admin_breadcrumb << {:label => id, :url => self.class.url("#{queue}/#{id}")}
+            render("admin/view_#{queue}")
         end
 
 
