cb4e07cbb997d991bc7fec552779fb63a40f4692
Ivan <ivan.pirlik@soluzionipa.it>
2011-12-22 20:02:29 +0100

    Admin fixes

--- apps/core/admin/controllers/app_admin_controller.rb
+++ apps/core/admin/controllers/app_admin_controller.rb
@@ -3,6 +3,7 @@ module Spider; module Admin
     class AppAdminController < Spider::PageController
 
         def before(action='', *params)
+            @scene.admin_breadcrumb ||= []
             unless @_did_breadcrumb
                 @scene.admin_breadcrumb << {:url => self.class.http_url, :label => self.class.app.full_name}
             end
