4a5e3d83edaecad3b60b9674e7d0cdc5de4a63d6
Ivan <ivan.pirlik@soluzionipa.it>
2012-02-06 15:03:10 +0100

    Ensure 'sort' is a Hash

--- apps/core/components/widgets/table/table.rb
+++ apps/core/components/widgets/table/table.rb
@@ -30,7 +30,7 @@ module Spider; module Components
         def prepare(action='')
             @model ||= @queryset.model
             @model = const_get_full(@model) if @model.is_a?(String)
-            if params['sort']
+            if params['sort'] && params['sort'].is_a?(Hash)
                 @sort_el = params['sort'].keys.first.to_sym 
                 @sort_dir = params['sort'].values.first.to_sym
                 @page = 1
