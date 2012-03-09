5d7d49fbada5fffb32ae922221a45a5cd9b27a97
Ivan <abmajor7@gmail.com>
2012-03-09 15:41:18 +0400

    Deleted old tests

--- test/init.rb
+++
@@ -1,8 +0,0 @@
-Spider.load_apps 'zoo'
-Spider.route_apps
-
-def Spider.test_teardown
-    Spider::Logger.debug("TEST TEARDOWN!!!!")
-    File.unlink(Spider.paths[:root]+'/var/db.test.sqlite')
-end
-
