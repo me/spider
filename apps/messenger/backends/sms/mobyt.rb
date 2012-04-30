93978644a1b4281a302e66b48c8def549da9e2bf
fabiano <fabiano.pavan@soluzionipa.it>
2012-04-30 12:27:17 +0200

    Conversione da utf-8 a iso e contatore messaggi multipli su file

--- apps/messenger/backends/sms/mobyt.rb
+++ apps/messenger/backends/sms/mobyt.rb
@@ -16,16 +16,24 @@ module Spider; module Messenger; module Backends; module SMS
             from = Spider.conf.get('messenger.mobyt.from')    
             to = msg.to
             text = msg.text
-            #trasformo gli accenti 
-            text = Spider::Messenger::Mobyt.trasforma_accenti(text)
             uri = URI('http://smsweb.mobyt.it/sms-gw/sendsmart')
             
             testi = Hash.new
             #se mando messaggio lungo spezzo in più messaggi
             if text.size > 160
+                percorso_contatore = File.join(Spider.paths[:var], "contatore_mobyt")
+                sequence_index = ""
+                File.open(percorso_contatore, File::RDWR|File::CREAT) do |f|
+                    f.flock(File::LOCK_EX)
+                    sequence_index = f.read.to_i + 1
+                    f.rewind
+                    sequence_index = sequence_index % 99
+                    f.write("#{sequence_index}")                        
+                    f.flush
+                    f.truncate(f.pos)
+                end
                 tot_response = true
                 operation="MULTI"
-                
                 cnt = 0
                 index = 1
                 mes = ""
@@ -38,8 +46,12 @@ module Spider; module Messenger; module Backends; module SMS
                 testi[tot_testi] = testi[tot_testi].strip
                 #invio sms con testi in hash testi
                 testi.each{ |key,testo|
+                    sequence_index = sequence_index.to_s.rjust(2, '0')
+                    tot_testi = tot_testi.to_s.rjust(2, '0')
+                    key = key.to_s.rjust(2, '0')
                     # udh=aabbcc: aa=id sequenza, bb=tot messaggi, cc=id del messaggio nella sequenza
-                    udh = "010"+tot_testi.to_s+"0"+key.to_s  
+                    udh = "#{sequence_index}#{tot_testi}#{key}"  
+                    puts "*******#{udh}"
                     uri_params = Spider::Messenger::Mobyt.parametri(username,password,to,from,testo,operation,udh)
                     response = Spider::Messenger::Mobyt.do_post_request(uri, uri_params)
                     Spider::Messenger::Mobyt.check_response_http(response)
