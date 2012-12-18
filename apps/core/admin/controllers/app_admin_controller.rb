da6f6fd2b942da5817d2e62e18cffb6ba3c6f354
Ivan <abmajor7@gmail.com>
2012-12-18 16:48:38 +0000

    Don't check users for login page

--- apps/core/admin/controllers/app_admin_controller.rb
+++ apps/core/admin/controllers/app_admin_controller.rb
@@ -10,15 +10,17 @@ module Spider; module Admin
             end
             @_did_breadcrumb = true
             super
-            our_app = Spider::Admin.apps[self.class.app.short_name]
-            raise "Admin #{self.class.app.short_name} not configured" unless our_app
-            user_classes = our_app[:options][:users] || Spider::Admin.base_allowed_users
-            unless user_classes.include?(@request.user.class)
-                raise Spider::Auth::Unauthorized.new(_("User not authorized to access this application"))
-            end
-            if our_app[:options][:check]
-                unless our_app[:options][:check].call(@request.user)
+            unless check_action(action, :login)
+                our_app = Spider::Admin.apps[self.class.app.short_name]
+                raise "Admin #{self.class.app.short_name} not configured" unless our_app
+                user_classes = our_app[:options][:users] || Spider::Admin.base_allowed_users
+                unless user_classes.include?(@request.user.class)
                     raise Spider::Auth::Unauthorized.new(_("User not authorized to access this application"))
+                end
+                if our_app[:options][:check]
+                    unless our_app[:options][:check].call(@request.user)
+                        raise Spider::Auth::Unauthorized.new(_("User not authorized to access this application"))
+                    end
                 end
             end
         end
